7025378fee49c8300e375a7f93d295d27e02f050
Justin Ormont <justinormont@users.noreply.github.com>
2019-09-13 12:24:24 -0700

    Moved to style tag, fixed flicking DOM elements, fixed hiding of primary page content

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+'use strict';
+
 (()=>{
     const unlockScroll = (dom) => {
         var style = window.getComputedStyle(dom ,null);
+
         if (style.getPropertyValue('overflow') === 'hidden') {
             document.documentElement.style.overflow = 'visible !important';
         }
@@ -16,25 +19,31 @@
 
     setInterval(unlockDocumentScroll, 1000);
     unlockDocumentScroll();
-    
+
+    const styleNode = document.createElement('style');
+    styleNode.innerHTML = '.hiddenSticky { display: none; }\n';
+    document.head.appendChild(styleNode);
+
     const getStickyDOM = (previous) => {
-        const allDOM = document.body.getElementsByTagName("*");
-        const newStickyDOM = [...allDOM].filter(a => { 
-            const pos = window.getComputedStyle(a, null).getPropertyValue('position'); 
-            return (pos === 'fixed' || pos === 'sticky');
+        const allDOM = document.body.getElementsByTagName('*');
+        const bodyHeight = document.body.clientHeight;
+        const newStickyDOM = [...allDOM].filter(a => {
+            const pos = window.getComputedStyle(a, null).getPropertyValue('position');
+            return ((pos === 'fixed' || pos === 'sticky') && a.getBoundingClientRect().height * 2 <= bodyHeight);
         });
         return [...new Set(newStickyDOM.concat(previous||[]))];
     }
-    
+
     let stickyDOM;
     let last = 1;
-    let prevScrollpos = window.pageYOffset;
+    let prevScrollPos = window.pageYOffset;
     const onScroll = () => {
         const currentScrollPos = window.pageYOffset;
-        if (Math.abs(prevScrollpos - currentScrollPos) > 20) {
-            if (prevScrollpos > currentScrollPos) {
+
+        if (Math.abs(prevScrollPos - currentScrollPos) > 20) {
+            if (prevScrollPos > currentScrollPos) {
                 if (last === 0) {
-                    stickyDOM.forEach(a => { if (a.style.display === 'none') { a.style.display = a.style.displayBack; } });
+                    stickyDOM.forEach(a => a.classList.remove('hiddenSticky'));
                     last = 1;
                     console.log('scroll up; display');
                 }
@@ -42,14 +51,15 @@
             else {
                 if (last === 1) {
                     stickyDOM = getStickyDOM(stickyDOM);
-                    stickyDOM.forEach(a => { if (a.style.display !== 'none') { a.style.displayBack = a.style.display; a.style.display = 'none'; } });
+                    stickyDOM.forEach(a => a.classList.add('hiddenSticky'));
                     last = 0;
                     console.log('scroll down; hide');
                 }
-                
             }
-            prevScrollpos = currentScrollPos;
+
+            setTimeout(() => { prevScrollPos = window.pageYOffset; }, 0); // Re-layout before saving the scroll position
         }
     }
-    window.addEventListener("scroll", onScroll);
+
+    window.addEventListener('scroll', onScroll);
 })();
